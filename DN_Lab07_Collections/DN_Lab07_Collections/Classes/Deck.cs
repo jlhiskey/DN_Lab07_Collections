ba6d580c896a94e65cae4435fe426c43250a11e8
Jason Hiskey <jasonhi@crazyredhead.com>
2019-01-15 20:59:16 -0800

    Framed out basic file structure and started README

---
+++ DN_Lab07_Collections/DN_Lab07_Collections/Classes/Deck.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DN_Lab07_Collections.Classes;
+
+namespace DN_Lab07_Collections.Classes
+{
+    public class Deck
+    {
+    }
+}
---
+++ DN_Lab07_Collections/Lab07TestSuite/UnitTest1.cs
@@ -0,0 +1,33 @@
+using System;
+using Xunit;
+using DN_Lab07_Collections.Classes;
+
+namespace Lab07TestSuite
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestingAddingCardToDeck()
+        {
+
+        }
+
+        [Fact]
+        public void TestingGettersSettersInCardClass()
+        {
+
+        }
+
+        [Fact]
+        public void RemoveCardFromDeck()
+        {
+
+        }
+
+        [Fact]
+        public void RemoveCardFromDeckThatDoesntExistInDeck()
+        {
+
+        }
+    }
+}
